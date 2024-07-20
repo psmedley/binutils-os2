64457383d0e12572ab34e865bf940ebe77d9eaef
Paul Smedley <paul@smedley.id.au>
2024-07-20 15:22:27 +1030

    Fix typo
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- bfd/i386aoutemx.c
+++ bfd/i386aoutemx.c
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #endif
 
 
-/* Avoid multiple defininitions from aoutx if supporting standarad a.out
+/* Avoid multiple defininitions from aoutx if supporting standard a.out
    as well as our own.  */
 /* Do not "beautify" the CONCAT* macro args.  Traditional C will not
    remove whitespace added here, and thus will fail to concatenate
